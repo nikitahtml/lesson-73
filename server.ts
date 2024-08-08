c4b06419bb37509160522c7e3d5bafc4810ac4c4
bibikov nikita <bibikovnikita777@gmail.com>
2024-08-08 18:35:51 +0600

    A server has been created that processes GET requests and returns an “echo” response. Implemented a route that takes a parameter from a URL and returns it as a response. Configured tsconfig.json to work with TypeScript.

--- server.ts
+++ server.ts
@@ -3,10 +3,12 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+
 app.get('/:message', (req: Request, res: Response) => {
     const { message } = req.params;
     res.send(message);
 });
+
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
